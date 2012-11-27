bb8b0f928ed29cefaa1c67f5ff7a8355b732ad83
Ciaran Archer <ciaran.archer@gmail.com>
2012-11-27 20:37:22 +0000

    Started views

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ end
 
 gem 'mongoid'
 gem 'sinatra'
+gem 'sinatra-contrib'
--- app/voter_app.rb
+++ app/voter_app.rb
@@ -1,9 +1,12 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 module Voter
   class App < Sinatra::Base
+    register Sinatra::Reloader
+
     get '/' do
-      'hello voter'
+      erb :home
     end
   end
 end
