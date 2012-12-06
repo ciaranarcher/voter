f81f2567f7894308f9cabaa4fdfb943be893bfa6
Ciaran Archer <ciaran.archer@gmail.com>
2012-12-06 20:23:04 +0000

    Added CSS

--- app/public/js/voter.js
+++ app/public/js/voter.js
@@ -5,17 +5,16 @@ var Voter = Voter || {};
 $(document).ready(function() {
   // create document
   Voter.myDOMHelper = new Voter.DOMHelper();
-  Voter.myDOMHelper.attachVoteButtons();
+  Voter.myDOMHelper.handleVoteSubmissions();
 });
 
 // create object
 Voter.DOMHelper = function() {};
 
 // attch buttons for clicking a vote
-Voter.DOMHelper.prototype.attachVoteButtons = function() {
+Voter.DOMHelper.prototype.handleVoteSubmissions = function() {
   
   $('#vote_options input').click(function () {
-    console.log('clicked');
     var option = $(this).attr('data-value');
     var participant_name = $('#participant_name').text();
     var participant_email = $('#participant_email').text();
