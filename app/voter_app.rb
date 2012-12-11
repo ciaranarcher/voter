2f429213caa6c56c805f096876a9392cfa91f03c
Ciaran Archer <ciaran.archer@gmail.com>
2012-12-11 19:58:16 +0000

    Added ranking page.

--- app/voter_app.rb
+++ app/voter_app.rb
@@ -8,6 +8,7 @@ module Voter
   class App < Sinatra::Base
     configure :development do
       register Sinatra::Reloader
+      also_reload 'lib/officer.rb'
     end
 
     configure do
@@ -30,7 +31,7 @@ module Voter
       @participant_email = @params[:participant_email]
 
       # find the topic by key
-      @topic = Officer::find_topic_by_key @params[:key]
+      @topic = Officer::find_topic_by_key params[:key]
       unless @topic.nil?
         @vote_options = @topic.vote_options
         erb :vote_on_topic
@@ -45,7 +46,7 @@ module Voter
 
     post '/topic' do
       option_params = @params.select {|k, v| k.start_with? 'option'}
-      @key = Officer::create_topic(@params[:name], option_params, settings.salt)
+      @key = Officer::create_topic(params[:name], option_params, settings.salt)
 
       erb :topic_created
     end
@@ -54,13 +55,13 @@ module Voter
       content_type :json
       
       participant = Officer::find_or_create_participant(
-        @params[:participant_email], @params[:participant_name]
+        @params[:participant_email], params[:participant_name]
       )
 
-      topic = Officer::find_topic_by_key @params[:topic_key]
+      topic = Officer::find_topic_by_key params[:topic_key]
 
       begin
-        if Officer::vote!(topic, participant, @params[:option])
+        if Officer::vote!(topic, participant, params[:option])
           {:success => true}.to_json
         else
           {:success => false, :reason => 'unknown'}.to_json
@@ -71,7 +72,13 @@ module Voter
     end
 
     get '/voted/:topic_key' do
-      'voted on ' + @params[:topic_key]
+      @topic = Officer::find_topic_by_key params[:topic_key]
+      if @topic.nil?
+        erb :topic_not_found
+      else
+        @ranked_options = Officer.report_ranked_desc @topic
+        erb :vote_results
+      end
     end
   end
 end
